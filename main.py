3ac4d51ee701b41c8764539a5bce8e584f80c825
Theriley106 <christopherlambert106@gmail.com>
2018-05-31 17:53:44 -0400

    Added main.py to revamp this project...

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+AMAZON_URL = "https://www.amazon.com/s/search-alias%3Dtradein-aps&field-keywords={0}&page={1}"
+ITEM_SELECTOR = ".s-item-container"
+TRADE_IN_SELECTOR = ".a-color-price"
+ITEM_SPECIFICS = ".a-text-left.a-col-right"
+# Page count = [0], Publisher = [1], ISBN-100 = [2], ISBN-13 = [3]
+BOOK_TITLE = ".s-access-title"
+BOOK_COVER = ".cfMarker"
+TRADE_IN_REVIEW_BOX = ".a-span-last"
+
+
+def isTradeInEligible(item):
+	# Determines if the item is trade in eligible or not
+	return ('tradein' in item.select(TRADE_IN_REVIEW_BOX)[0])
+
+
