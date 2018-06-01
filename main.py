b0bd6c2b9b5005616af105d89ec8c30d9d464d1b
Theriley106 <christopherlambert106@gmail.com>
2018-05-31 21:12:10 -0400

    Cover works now

--- main.py
+++ main.py
@@ -43,13 +43,64 @@ def grabPage(url):
 	print("Grabbed: {} | Page: {}".format(page.title.string, pageNum))
 	return page
 
+def extractInfoFromItem(item):
+	try:
+		tempInfo = {}
+		tempInfo['title'] = item.select(BOOK_TITLE)[0].getText()
+		tempInfo['cover'] = str(item.select(BOOK_COVER)[0]).partition('src="')[2].partition('"')[0]
+		tempInfo['page_count'] = item.select(ITEM_SPECIFICS)[0].getText()
+		tempInfo['publisher'] = item.select(ITEM_SPECIFICS)[1].getText()
+		tempInfo['isbn_100'] = item.select(ITEM_SPECIFICS)[2].getText()
+		tempInfo['isbn_13'] = item.select(ITEM_SPECIFICS)[3].getText()
+		tempInfo['trade_in_price'] = item.select(TRADE_IN_SELECTOR)[0].getText()
+	except:
+		tempInfo = None
+	return tempInfo
+
+def extractInfoFromPage(page):
+	pageItems = []
+	for item in page.select(ITEM_SELECTOR):
+		info = extractInfoFromItem(item)
+		if info != None:
+			pageItems.append(info)
+	return pageItems
+
+def extractInfoFromURL(url):
+	page = grabPage(url)
+	return extractInfoFromPage(page)
+
+class search(object):
+	def __init__(self):
+		self.results = []
+
+	def query(self, keyword):
+		url = AMAZON_URL.format(keyword, 1)
+		page = grabPage(url)
+		pageCount = getPageCount(page)
+		print("Keyword: {} Pages: {}".format(keyword, pageCount))
+		for i in range(1, pageCount):
+			if i != 1:
+				url = AMAZON_URL.format(keyword, i)
+				page = grabPage(url)
+
+
+
 
 class amazonTextbookDB(object):
 	def __init__(self, arg):
 		self.arg = arg
 		self.database = []
 
-	#def search(self, keyword):
+	def search(self, keyword):
+		url = AMAZON_URL.format(keyword, 1)
+		page = grabPage(url)
+		pageCount = getPageCount(page)
+		print("Keyword: {} Pages: {}".format(keyword, pageCount))
+		for i in range(1, pageCount):
+			if i != 1:
+				url = AMAZON_URL.format(keyword, i)
+				page = grabPage(url)
+
 
 
 	#def search(self, keyword):
@@ -60,6 +111,7 @@ if __name__ == '__main__':
 	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+2&rh=i%3Atradein-aps%2Ck%3Apython+2"
 	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+program&rh=i%3Atradein-aps%2Ck%3Apython+program"
 	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+3&rh=i%3Atradein-aps%2Ck%3Apython+3"
+	url = "https://www.amazon.com/s/ref=sr_pg_2?&fst=p90x%3A1&rh=i%3Atradein-aps%2Ck%3Atextbooks&page=2"
 	page = grabPage(url)
-	print getPageCount(page)
+	print extractInfoFromPage(page)
 	#print getResultCount(page).getText()
