945cae2f0a6c9f8a91af6d5e7d1e479fa890a901
Theriley106 <christopherlambert106@gmail.com>
2018-05-31 20:38:07 -0400

    Added correct get page count

--- main.py
+++ main.py
@@ -19,8 +19,16 @@ def isTradeInEligible(item):
 	# Determines if the item is trade in eligible or not
 	return ('tradein' in item.select(TRADE_IN_REVIEW_BOX)[0])
 
-def getResultCount(page):
-	return page.select("#s-result-count")[0]
+def getPageCount(page):
+	try:
+		pageCount = int(page.select(".pagnDisabled")[0].getText())
+	except:
+		try:
+			pageCount = int(re.findall("(\d+)", str(page.select("#pagn")[0].getText().replace("\n", " ")))[-1])
+		except:
+			pageCount = 1
+	return pageCount
+
 
 def grabPage(url):
 	for i in range(3):
@@ -31,7 +39,7 @@ def grabPage(url):
 	try:
 		pageNum = re.findall('page\S(\d+)', url)[0]
 	except:
-		pageNum = 0
+		pageNum = 1
 	print("Grabbed: {} | Page: {}".format(page.title.string, pageNum))
 	return page
 
@@ -39,11 +47,19 @@ def grabPage(url):
 class amazonTextbookDB(object):
 	def __init__(self, arg):
 		self.arg = arg
+		self.database = []
+
+	#def search(self, keyword):
+
 
 	#def search(self, keyword):
 
 
 if __name__ == '__main__':
 	url = "https://www.amazon.com/s/ref=sr_nr_i_0?srs=9187220011&fst=as%3Aoff&rh=i%3Atradein-aps%2Ck%3Abiology%2Ci%3Astripbooks&keywords=biology&ie=UTF8&qid=1527811678"
-	url = "https://www.amazon.com/s/ref=sr_pg_2?srs=9187220011&fst=as%3Aoff&rh=n%3A283155%2Ck%3Abiochemistry&page=2&keywords=biochemistry&ie=UTF8&qid=1527811953"
-	grabPage(url)
+	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+2&rh=i%3Atradein-aps%2Ck%3Apython+2"
+	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+program&rh=i%3Atradein-aps%2Ck%3Apython+program"
+	#url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=srs%3D9187220011%26search-alias%3Dtradein-aps&field-keywords=python+3&rh=i%3Atradein-aps%2Ck%3Apython+3"
+	page = grabPage(url)
+	print getPageCount(page)
+	#print getResultCount(page).getText()
